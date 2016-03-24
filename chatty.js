03ac71326beab0fd0286bdb14b418c450e2ed072
Dhruti Upender <dhruti.upender@gmail.com>
2016-03-23 21:38:00 -0500

    added css and dark theme

--- chatty.js
+++ chatty.js
@@ -25,7 +25,13 @@ var Chatty = (function() {
       }
      }
     }
-      
   })();
       
   Chatty.loadMessages(Chatty.showMessages);
+
+
+// When the user clicks on the dark theme checkbox, change the background color of your application to a dark gray, and the font color for messages should be white(ish)... you pick.
+// If the user unchecks the box, the background color should change back to white with black text for messages.
+  document.getElementById("dark").addEventListener("click", function() {
+  document.getElementById("mContainer").classList.toggle("darkTheme2");   
+  });
--- eventlistener.js
+++ eventlistener.js
@@ -15,3 +15,9 @@ function keyEvent(){
     //   let newBio = event.currentTarget.value;
     //   currBio.innerHTML = newBio;
     //   console.log(event );
+
+// When the user clicks on the dark theme checkbox, change the background color of your application to a dark gray, and the font color for messages should be white(ish)... you pick.
+// If the user unchecks the box, the background color should change back to white with black text for messages.
+// document.getElementById("dark").addEventListener("click", function() {
+//     document.getElementById("mContainer").classList.add("darkTheme");   
+// });
